8e35d55ac93a7972093144edf64592cd26c0b85b
KaPhuDong <dong.ka26@student.passerellesnumeriques.org>
2025-02-26 22:10:29 +0700

    done

--- app/Http/Controllers/CovidController.php
+++ app/Http/Controllers/CovidController.php
@@ -9,9 +9,7 @@ class CovidController extends Controller
 {
     public function getData()
     {
-
-
-        $response = Http::withOptions(['verify' => false])->get('https://jsonplaceholder.typicode.com/posts');
+        $response = Http::get('https://jsonplaceholder.typicode.com/posts');
 
         $data = $response->json();
         return view('covid')->with('data', $data);
